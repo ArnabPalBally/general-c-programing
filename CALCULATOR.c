f71b8239575744546cee4fc1bba998e8520e7665
ArnabPalBally <77872764+ArnabPalBally@users.noreply.github.com>
2021-01-25 22:53:55 +0530

    Add files via upload
    This is basic c code which is implemented on the logic of calculator

---
+++ CALCULATOR.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	float a,b;
+	printf("ENTER THE VALUE OF a :");
+	scanf("%f",&a);
+	printf("ENTER THE VALUE OF b :");
+	scanf("%f",&b);
+	float sum=a+b;
+	float subtract=a-b;
+	float multiplication=a*b;
+	float divide=a/b;
+	printf("THE SUM IS =%f\n",sum);
+	printf("THE SUBTRACT IS =%f\n",subtract);
+	printf("THE MULTIPLICATION IS =%f\n",multiplication);
+	printf("THE SUBTRACT IS =%f\n",divide);
+	return 0;
+}
